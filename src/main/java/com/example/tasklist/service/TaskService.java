c333f050a33a6065cde82362d38cf9511e3ee085
Maxim Dziuba <dziuba200517@gmail.com>
2023-04-17 21:33:28 +0200

    first commit

---
+++ src/main/java/com/example/tasklist/service/TaskService.java
@@ -0,0 +1,19 @@
+package com.example.tasklist.service;
+
+import com.example.tasklist.domain.task.Task;
+
+import java.util.List;
+
+public interface TaskService {
+
+    Task getById(Long id);
+
+    List<Task> getAllByUserId(Long userId);
+
+    Task update(Task task);
+
+    Task create(Task task, Long userId);
+
+    void delete(Long id);
+
+}
